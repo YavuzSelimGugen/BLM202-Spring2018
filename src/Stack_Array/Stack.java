820bc6b8ea7e2d6cf90b1dc92a51cc5c1952b7ca
Muhammet Alkan <muhammetalkan92@marun.edu.tr>
2018-03-16 15:46:04 +0300

    Stack using array - 16.03.2018

---
+++ src/Stack_Array/Stack.java
@@ -0,0 +1,49 @@
+package Stack_Array;
+
+/**
+ * @file Stack.java
+ * @date Mar 16, 2018 , 2:14:38 PM
+ * @author Muhammet Alkan
+ */
+public class Stack<T> {
+
+    private T[] dataArray;
+    private int top;
+
+    public Stack(int maxSize) {
+        dataArray = (T[]) new Object[maxSize];
+        top = -1;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public void push(T data) {
+        if (top == dataArray.length - 1) {
+            System.out.println("list is full !");
+        } else {
+            // top++;
+            dataArray[++top] = data;
+        }
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            System.out.println("list is empty !");
+            return null;
+        } else {
+            //T temp = dataArray[top];
+            //top--;
+            //return temp;
+            return dataArray[top--];
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i <= top; i++) {
+            System.out.println(dataArray[i]);
+        }
+    }
+
+}
